f2856d480d6af60f491e144775f6447513a2c599
Jacek Panasiuk <panasiuk.jc@gmail.com>
2023-03-01 12:58:39 +0100

    Render a simple progression grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { type CSSProperties } from "react";
+import { days } from "./days";
+import { levels } from "./levels";
+import { progression } from "./progression";
 import "./App.css";
 
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <Grid />
+    </main>
+  );
 }
+
+function Grid() {
+  return (
+    <div className="Grid" style={gridStyle}>
+      {levels.map((level) => (
+        <div key={level} className="GridRow">
+          {days.map((day) => (
+            <div key={`${level}/${day}`} className="GridCell">
+              {progression[level].includes(day) ? level : null}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const gridStyle = {
+  "--grid-rows": levels.length,
+  "--grid-cols": days.length,
+} as CSSProperties;
 
 export default App;
