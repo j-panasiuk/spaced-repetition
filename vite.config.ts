6ac57b8f22393dd23438d2c72e00b0d99608508c
Jacek Panasiuk <panasiuk.jc@gmail.com>
2023-03-01 10:42:20 +0100

    Setup vitest for unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsConfigPaths()],
+  define: {
+    "import.meta.vitest": false,
+  },
+  // @ts-ignore
+  test: {
+    includeSource: ["src/**/*.{js,ts}"],
+  },
+});
